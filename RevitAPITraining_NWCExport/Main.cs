f35aa2f3809d0c0053225c8ce81a5f12d4a0f9a6
Dmitriy Baranov <bdvbuild@gmail.com>
2023-02-15 19:20:39 +0400

    Добавьте файлы проекта.

---
+++ RevitAPITraining_NWCExport/Main.cs
@@ -0,0 +1,25 @@
+﻿using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using System;
+using System.Transactions;
+
+namespace RevitAPITraining_NWCExport
+{
+    [Transaction(TransactionMode.Manual)]
+    public class Main : IExternalCommand
+    {
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            Document doc = commandData.Application.ActiveUIDocument.Document;
+
+            string path = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+            string name = "export.nwc";
+            NavisworksExportOptions nwcOption = new NavisworksExportOptions();
+
+            doc.Export(path, name, nwcOption);
+
+            return Result.Succeeded;
+        }
+    }
+}
